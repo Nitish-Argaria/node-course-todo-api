fbfabce043a93ee15e0451c89ea1c3e53c099567
argaria <argaria@Nitishs-MacBook-Air.local>
2018-08-04 16:23:20 +0530

    Test POSY /todo route

--- server/server.js
+++ server/server.js
@@ -11,20 +11,23 @@ app.use(bodyParser.json());
 
 app.post('/todos',(req,res)=>{
 	console.log(req.body);
+	
 	var todo = new Todo({
 		text : req.body.text
 	});
 
-todo.save().then((doc)=>{
-	res.send(doc);
+	todo.save().then((doc)=>{
+		res.send(doc);
 
-},(e)=>{
-	res.status(400).send(e);
-});
+		},(e)=>{
+		res.status(400).send(e);
+	});
 });
 app.listen(3000,()=>{
 	console.log('Started on port 3000');
 });
+
+module.exports = {app};
 // var newUser = new User({
 // 	email:' argaria.nitish@gmail.com'
 // }) ; 
